bbd292a3a41ff36c24f09f0090fe1526cdabf375
nicholas-gs <nicholasganshyan@gmail.com>
2022-08-24 20:31:50 +0800

    add dt_calibration_utils package

---
+++ dt_calibration_utils/dt_calibration_utils/calibration_file.py
@@ -0,0 +1,39 @@
+import os
+import time
+import yaml
+
+from os.path import expanduser
+
+
+HOME_DIR = expanduser("~")
+BASE_DIR = os.path.join(HOME_DIR, ".dt-cps/data/calibrations")
+
+
+def save_calibration(rel_file_path, data) -> str:
+    """Save data into YAML file
+    Returns the full path to the written YAML file.
+    """
+    data["calibration_time"] = time.strftime("%Y-%m-%d-%H-%M-%S")
+    full_fp = os.path.join(BASE_DIR, rel_file_path)
+    try:
+        os.makedirs(os.path.dirname(full_fp))
+    except OSError as _:
+        pass
+    with open(full_fp, 'w') as outfile:
+        outfile.write(yaml.safe_dump(data, default_flow_style=False))
+    return full_fp
+
+def read_calibration(rel_file_path: str, vars=None):
+    full_fp = os.path.join(BASE_DIR, rel_file_path)
+    if not os.path.isfile(full_fp):
+        return None, full_fp, None
+    else:
+        with open(full_fp, 'r') as in_file:
+            try:
+                data = yaml.safe_load(in_file)
+                if vars is None:
+                    return data, full_fp, None
+                else:
+                    return {var : data.get(var) for var in vars}, full_fp, data.get("calibration_time")
+            except yaml.YAMLError as _:
+                return None, full_fp, None
---
+++ dt_calibration_utils/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'dt_calibration_utils'
+
+setup(
+    name=package_name,
+    version='1.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='nicholas-gs',
+    maintainer_email='nicholasganshyan@gmail.com',
+    description='Utility package for reading and writing to calibration files',
+    license='GPLv3',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
