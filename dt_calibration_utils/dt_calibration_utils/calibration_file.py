65bb190d360fbdc99e1eb030649144db8e26fb49
nicholas-gs <nicholasganshyan@gmail.com>
2022-10-12 13:06:26 +0800

    simplify  function

--- dt_calibration_utils/dt_calibration_utils/calibration_file.py
+++ dt_calibration_utils/dt_calibration_utils/calibration_file.py
@@ -3,6 +3,7 @@ import time
 import yaml
 
 from os.path import expanduser
+from typing import Any, Tuple, Optional
 
 
 __all__ = [
@@ -29,17 +30,26 @@ def save_calibration(rel_file_path, data) -> str:
         outfile.write(yaml.safe_dump(data, default_flow_style=False))
     return full_fp
 
-def read_calibration(rel_file_path: str, vars=None):
+
+def read_calibration(rel_file_path: str) -> Tuple[Optional[Any], str]:
+    """Read the calibration file and return its contents.
+
+    :param rel_file_path: Relative file path to the YAML calibration file.
+        For example, `rel_file_path` can be intrinsic/default.yaml. 
+    :type rel_file_path: str
+    :return: The first element in the tuple contents the data in the YAML file.
+        It will be `None` if unable to read in the data. The second element in
+        the tuple is the full constructed file path to the YAML file.
+    :rtype: Tuple[Optional[Any], str]
+    """
+    assert(rel_file_path.endswith('.yaml'))
     full_fp = os.path.join(BASE_DIR, rel_file_path)
     if not os.path.isfile(full_fp):
-        return None, full_fp, None
+        return None, full_fp
     else:
         with open(full_fp, 'r') as in_file:
             try:
                 data = yaml.safe_load(in_file)
-                if vars is None:
-                    return data, full_fp, None
-                else:
-                    return {var : data.get(var) for var in vars}, full_fp, data.get("calibration_time")
+                return data, full_fp
             except yaml.YAMLError as _:
-                return None, full_fp, None
+                return None, full_fp
